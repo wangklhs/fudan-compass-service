57abd0775e47ab42b8d346f70898ddc3a9ec440b
wangshuai <19302010050@fudan.edu.cn>
2022-11-14 19:10:09 +0800

    init fudan-compass-service

---
+++ src/main/java/fudan/pm/fudanCompass/FudanCompassApplication.java
@@ -0,0 +1,13 @@
+package fudan.pm.fudanCompass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudanCompassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FudanCompassApplication.class, args);
+    }
+
+}
