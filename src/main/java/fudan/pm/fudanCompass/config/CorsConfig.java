129273a0aebb91b85dd99ded02c1c1ae2bf31c23
wangshuai <19302010050@fudan.edu.cn>
2022-12-08 15:43:32 +0800

    跨域

---
+++ src/main/java/fudan/pm/fudanCompass/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package fudan.pm.fudanCompass.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .maxAge(3600)
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
