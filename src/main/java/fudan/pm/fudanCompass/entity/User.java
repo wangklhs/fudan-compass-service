54f058b2ee00773b92706fe40db3ebd14fd4380d
LesuTree <19302010043@fudan.edu.cn>
2022-12-14 13:09:07 +0800

    "修复课程表长度上限为3000"

--- src/main/java/fudan/pm/fudanCompass/entity/User.java
+++ src/main/java/fudan/pm/fudanCompass/entity/User.java
@@ -2,10 +2,7 @@ package fudan.pm.fudanCompass.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity
@@ -19,6 +16,7 @@ public class User {
     private String major;
 
     //标识符进行区分，天数之间为*，课程之间为#
+    @Column(length = 3000)
     private String courseTable=" & & & & & & ";
     public User(String username, String password,String major) {
         this.username = username;
